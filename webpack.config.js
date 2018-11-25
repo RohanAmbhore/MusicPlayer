1bafd94fa2692e10e5f5ad19680865453f5b7814
RohanAmbhore <rohanambhore01@gmail.com>
2018-11-25 20:21:38 +0530

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { 
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                     },
+                     { 
+                         loader: 'postcss-loader',
+                         options: {
+                             ident: 'postcss',
+                             plugins: () => [
+                                 autoprefixer({
+                                     browsers: [
+                                        "> 1%",
+                                        "last 2 versions"
+                                     ]
+                                 })
+                             ]
+                         }
+                      }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'syle-loader'
+                    }, {
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.HashedModuleIdsPlugin()// This will keep the vendor chunk to have same version
+    ],
+    optimization: {
+        runtimeChunk: 'single', // Split out runtime code into separate chunk 
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[//\]node_modules[\\/]/, // Split out vendor code into separate chunk
+                    name: 'vendors',
+                    chunks: 'all'
+                },
+            commons: {
+                name: "commons", // Split code common to all chunks to its own chunk
+                chunks: "initial",
+                minChunks: 2
+            }
+            }
+        }
+    }
+}
